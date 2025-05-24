4babadb00d47260a2fc83a6becbe4c0a2f262f76
FoyMason <149638453+FoyMason@users.noreply.github.com>
2025-05-24 16:31:00 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5175,
+    host: true,
+    allowedHosts: ['purerankstrategies.com']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
